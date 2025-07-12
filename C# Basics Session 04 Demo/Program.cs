5d8aee6316459f60129b0322a845bf8ee0f238a6
Mahmoud Mansour <mansour@gmail.com>
2025-07-12 22:15:51 +0300

    Complete Second Video - Part 02 GOTO

--- C# Basics Session 04 Demo/Program.cs
+++ C# Basics Session 04 Demo/Program.cs
@@ -4,116 +4,153 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World");
-            #region Control Statements
+        //Console.WriteLine("Hello World");
+        #region Control Statements
 
-            #region 1. Conditional Statements
+        #region 1. Conditional Statements
 
-            #region Example 01 [Year Quarter] - [If, Switch with Neumric Type Using Constant Pattern] 
+        #region Example 01 [Year Quarter] - [If, Switch with Neumric Type Using Constant Pattern] 
 
-            //Console.Write("Please Enter Month A Number Existed in 1st Quarter: ");
-            //bool FlagMonthNumber = int.TryParse(Console.ReadLine(), out int MonthNumber);
+        //Console.Write("Please Enter Month A Number Existed in 1st Quarter: ");
+        //bool FlagMonthNumber = int.TryParse(Console.ReadLine(), out int MonthNumber);
 
-            #region If, Else If, Else Solution
+        #region If, Else If, Else Solution
 
-            //if (MonthNumber == 1)
-            //    Console.WriteLine("Month is Jan");
-            //else if (MonthNumber == 2)
-            //    Console.WriteLine("Month is Feb");
-            //else if (MonthNumber == 3)
-            //    Console.WriteLine("Month is Mar");
-            //else
-            //    Console.WriteLine("Please enter valid Number");
+        //if (MonthNumber == 1)
+        //    Console.WriteLine("Month is Jan");
+        //else if (MonthNumber == 2)
+        //    Console.WriteLine("Month is Feb");
+        //else if (MonthNumber == 3)
+        //    Console.WriteLine("Month is Mar");
+        //else
+        //    Console.WriteLine("Please enter valid Number");
 
-            #endregion
+        #endregion
 
-            #region Switch Solution
+        #region Switch Solution
 
-            //switch(MonthNumber)
-            //{
-            //    case 1: 
-            //        Console.WriteLine("Month is Jan");
-            //        break;
-            //    case 2:
-            //        Console.WriteLine("Month is Feb");
-            //        break;
-            //        case 3:
-            //        Console.WriteLine("Month is Mar");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Please enter Valid Number");
-            //        break;
-            //}
+        //switch(MonthNumber)
+        //{
+        //    case 1: 
+        //        Console.WriteLine("Month is Jan");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Month is Feb");
+        //        break;
+        //        case 3:
+        //        Console.WriteLine("Month is Mar");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Please enter Valid Number");
+        //        break;
+        //}
 
-            #endregion
+        #endregion
 
-            #endregion
+        #endregion
 
-            #region Example 02 [Student Age] - [If, Switch with Numric type using Relational Pattern]
-            /// Age is Greater than 22 => Student Age is Greater than 22
-            /// Age is less than 22 => Student Age is less than 22
-            /// Age is 22 => Student Age is 22
+        #region Example 02 [Student Age] - [If, Switch with Numric type using Relational Pattern]
+        /// Age is Greater than 22 => Student Age is Greater than 22
+        /// Age is less than 22 => Student Age is less than 22
+        /// Age is 22 => Student Age is 22
 
-            //Console.Write("Please Enter Student Age: ");
-            //int.TryParse(Console.ReadLine(), out int age );
+        //Console.Write("Please Enter Student Age: ");
+        //int.TryParse(Console.ReadLine(), out int age );
 
-            #region If, Else if, Else Solution
-            //if (age > 22)
-            //    Console.WriteLine("Student Age is Greater than 22");
-            //else if (age < 22)
-            //    Console.WriteLine("Student Age is less than 22");
-            //else
-            //    Console.WriteLine("Student Age is 22");
-            #endregion
+        #region If, Else if, Else Solution
+        //if (age > 22)
+        //    Console.WriteLine("Student Age is Greater than 22");
+        //else if (age < 22)
+        //    Console.WriteLine("Student Age is less than 22");
+        //else
+        //    Console.WriteLine("Student Age is 22");
+        #endregion
 
-            #region Switch Solution
-            //switch (age)
-            //{
-            //    case > 22:
-            //        Console.WriteLine("Student Age is Greater than 22");
-            //        break;
-            //    case < 22:
-            //        Console.WriteLine("Student Age is less than 22");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Student Age is 22");
-            //        break;
-            //}
-            #endregion
+        #region Switch Solution
+        //switch (age)
+        //{
+        //    case > 22:
+        //        Console.WriteLine("Student Age is Greater than 22");
+        //        break;
+        //    case < 22:
+        //        Console.WriteLine("Student Age is less than 22");
+        //        break;
+        //    default:
+        //        Console.WriteLine("Student Age is 22");
+        //        break;
+        //}
+        #endregion
 
-            #endregion
+        #endregion
 
-            #region Example 03 [Student Name] - [If, Switch with String Type]
-            /// name = Omar => Hello Omar
-            /// name = May => Hello May
-            /// name = Aliaa => Hello Aliaa
+        #region Example 03 [Student Name] - [If, Switch with String Type]
+        /// name = Omar => Hello Omar
+        /// name = May => Hello May
+        /// name = Aliaa => Hello Aliaa
 
-            //Console.Write("Enter Student Name: ");
-            //string name = Console.ReadLine() ?? "No Name Entered";
+        //Console.Write("Enter Student Name: ");
+        //string name = Console.ReadLine() ?? "No Name Entered";
 
-            #region If, Else If, Else Solution
-            //if (name == "Omar")
-            //    Console.WriteLine("Hello Omar");
-            //else if (name == "May")
-            //    Console.WriteLine("Hello May");
-            //else if (name == "Aliaa")
-            //    Console.WriteLine("Hello Aliaa");
-            #endregion
+        #region If, Else If, Else Solution
+        //if (name == "Omar")
+        //    Console.WriteLine("Hello Omar");
+        //else if (name == "May")
+        //    Console.WriteLine("Hello May");
+        //else if (name == "Aliaa")
+        //    Console.WriteLine("Hello Aliaa");
+        #endregion
 
-            #region Switch Solution
-            //switch(name)
-            //{
-            //    case "Omar":
-            //        Console.WriteLine("Hello Omar");
-            //        break;
-            //    case "May":
-            //        Console.WriteLine("Hello May");
-            //        break;
-            //    case "Aliaa":
-            //        Console.WriteLine("Hello Aliaa");
-            //        break;
-            //}
-            #endregion
+        #region Switch Solution
+        //switch(name)
+        //{
+        //    case "Omar":
+        //        Console.WriteLine("Hello Omar");
+        //        break;
+        //    case "May":
+        //        Console.WriteLine("Hello May");
+        //        break;
+        //    case "Aliaa":
+        //        Console.WriteLine("Hello Aliaa");
+        //        break;
+        //}
+        #endregion
+        #endregion
+
+        #region Example 04 [Budget] - [Switch with Goto]
+        /// Budget is 1000 => Option 01
+        /// Budget is 2000 => Option01, Option 02
+        /// Budget is 3000 => Option01, Option 02, Option 03
+
+        //Console.Write("Please Enter your Budget : ");
+        //int.TryParse(Console.ReadLine(), out int budget);
+
+        //switch(budget)
+        //{
+        //    case 3000:
+        //        Console.WriteLine("Option 03");
+        //        goto case 2000;
+        //        break;
+        //    case 2000:
+        //        Console.WriteLine("Option 02");
+        //        goto case 1000;
+        //        break;
+        //    case 1000:
+        //        Console.WriteLine("Option 01");
+        //        break;
+        //}
+
+        #endregion
+
+        #region Goto Example
+
+        //Retry:
+        //    Console.Write("Enter Name : ");
+        //    string name = Console.ReadLine() ?? "Invalid Name";
+
+        //    if (name == "Mostafa")
+        //        goto Retry;
+        //    else
+        //        Console.WriteLine($"Name is {name}");
             #endregion
 
             #endregion
