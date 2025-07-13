ceff5dbdf0154d8f76fa05574fe7fb594d55625a
Mahmoud Mansour <mansour@gmail.com>
2025-07-13 05:43:41 +0300

    Compleate 3rd Video - Evolution of switch in C# 7.0

---
+++ C# Basics Session 04 Demo/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Basics_Session_04_Demo
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
