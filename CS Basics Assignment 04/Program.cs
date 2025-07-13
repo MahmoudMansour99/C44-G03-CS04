7b81d287cf273383780f51be1365e2c1ea05608d
Mahmoud Mansour <mansour@gmail.com>
2025-07-13 06:35:17 +0300

    Add project files.

---
+++ CS Basics Assignment 04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basics_Assignment_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
